9f62ab109f4728d389065a28e1d03fd4a1ab97d1
Mifta Hamid <miftahamid@Miftas-Air.fios-router.home>
2018-12-10 01:42:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardFour);
+
+var cards =["queen","queen","king","king"];
+
+var cardsInPlay = [];
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+var cardOne = ["queen", 0 ];
+
+console.log("User flipped queen");
+
+var cardTwo = ["king", 2 ];
+
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay ) {
+   alert("You found a match");
+} else {
+  alert("Sorry, try again");
+}
